de9a79d71759cf1b70d60281c1cd4e3d5ad0ae0a
Rasel Hossain <hossain.rasel2002@gmail.com>
2025-07-19 00:56:20 +0600

    first

---
+++ test.py
@@ -0,0 +1,18 @@
+for i in range(5):
+    print(i+1 ,"hello world")
+print("hello world")
+print("hello world again")
+#print("hello world again and again")
+print("hello world for the last time")
+print("hello world for the last time, really")
+
+for i in range(5):
+    print("1", i+1)
+a = 14
+if (a%2 == 0):
+    print("a is equal to even number")
+else:
+    print("a is equal to odd number")
+
+print("This is a test file")
+print("This is a test file for the CI/CD pipeline")
