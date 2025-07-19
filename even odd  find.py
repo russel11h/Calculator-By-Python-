7265a9325d23e665625b3d9233925beadedc177e
Rasel Hossain <hossain.rasel2002@gmail.com>
2025-07-20 01:12:06 +0600

    main

---
+++ even odd  find.py
@@ -0,0 +1,9 @@
+
+print("Calculating Even or Odd Numbers")
+a=float (input("Enter Your Number : "))
+
+
+if (a%2 == 0):
+    print(str(a) + " is even number !")
+else:
+    print(str(a) + " is odd number !")
