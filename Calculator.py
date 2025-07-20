2ffaae0b04765a18a98e06be321a0de6b4131f17
Rasel Hossain <hossain.rasel2002@gmail.com>
2025-07-20 13:35:37 +0600

    main

--- Calculator.py
+++ Calculator.py
@@ -1,30 +1,30 @@
-print("Welcome to the Simple Calculator!")
+
 print("What is your name?")
 name = input("Enter your name: ")
 print("Hello", name + "!")
 
+print("Welcome to the Simple Calculator!")
 print("Please enter your symbol (+, -, *, /):")
 symbol = input("Enter your symbol: ")
 
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+
 if symbol == "+":
     print("You have selected addition")
-    a = float(input("Enter first number: "))
-    b = float(input("Enter second number: "))
+  
     print("Result:", a + b)
 elif symbol == "-":
     print("You have selected subtraction")
-    a = float(input("Enter first number: "))
-    b = float(input("Enter second number: "))
+
     print("Result:", a - b)
 elif symbol == "*":
     print("You have selected multiplication")
-    a = float(input("Enter first number: "))
-    b = float(input("Enter second number: "))
+
     print("Result:", a * b)
 elif symbol == "/":
     print("You have selected division")
-    a = float(input("Enter first number: "))
-    b = float(input("Enter second number: "))
+
     if b == 0:
         print("Error: Division by zero is not allowed")
     else:
